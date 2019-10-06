28a10b7660f9631059a929298dbc48e2f984d7b8
SSKhan <git@reseller.suryazi.com>
2019-10-06 06:07:18 +0300

    Removed Ribbon

--- second-service/src/main/java/com/bab/microservices/secondservice/FirstServiceProxy.java
+++ second-service/src/main/java/com/bab/microservices/secondservice/FirstServiceProxy.java
@@ -1,13 +1,11 @@
 package com.bab.microservices.secondservice;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.bab.microservices.secondservice.bean.Greetings;
 
 @FeignClient(name="first-service")
-@RibbonClient(name="first-service")
 public interface FirstServiceProxy {
 	
 	@GetMapping("/greetings")
